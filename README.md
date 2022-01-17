a24148d76297a847cf9fbf3f9579bbcefec6ea5e
xuan <xuan>
2022-01-17 17:39:41 +0800

    给地图增加标记点

--- README.md
+++ README.md
@@ -1,23 +1,55 @@
-## 创建并且创建地图
+## 给地图添加标记点
 
-1. 首先，引入 `esri-loader` 包，如下：
-```js
-import * as esriLoader from "esri-loader";
+### svg 可单个可多个
+
+1. 首先，需要在 [加载地图] 的基础上再引入以下资源：
+```diff
+"esri/map",
++ "esri/geometry/Point",
++ "esri/graphic",
++ "dojo/_base/array",
+"dojo/domReady!",
 ```
 
-2. 在 `methods` 里面进行 `arcgis` 的配置:
+2. 标记点的经纬度坐标
 ```js
-    // url：arcgis 的地图资源，3.39为 arcgis 的版本号
-    const options = {
-        url: "https://js.arcgis.com/3.39/",
-      };
-    
-    // 引入需要的资源文件，需要按照顺序引入，否则可能会报错（根据官网的顺序）
-    esriLoader.loadModules(["esri/map", "dojo/domReady!"], options).then(([Map]) => {
-        this.map = new Map("Map", {
-          basemap: "topo-vector", // arcgis 的地图样式
-          center: [19.82, 41.33], // arcgis 的中心点坐标
-          zoom: 3, // arcgis 的放大标准
-        });
-    });
+// 可以为单个或者多个
+let point = [
+  [19.82, 41.33],
+  [16.37, 48.21],
+];
+```
+
+3. 标记点图片和标记点的填充颜色
+```js
+// iconpath: svg 的 path 路径
+let iconpath =
+              "M819.6 606.7l-185.8-52.5-189 52.6-103.5-42.9-28.7 69.3L440 686l193.6-53.9 190.9 54 136.7-57.7-29.2-69.1zM819.6 820.9l-185.8-52.5-189 52.6-103.5-42.8-28.7 69.2L440 900.2l193.6-53.9 190.9 54 136.7-57.7-29.2-69.1zM633.6 203.7l190.9 54 136.7-57.8-29.2-69.1-112.4 47.5-185.8-52.5-189 52.6-103.5-42.9-28.7 69.3L440 257.6zM819.6 392.5L633.8 340l-189 52.6-103.5-42.9-28.7 69.3L440 471.8l193.6-53.9 190.9 54 136.7-57.8-29.2-69zM66.8 139.1h94.9v176.4H66.8v75h94.9v186.6H66.8v75h94.9v201.7H64.5v75h172.2V64.1H66.8z";
+// fill_color: 填充颜色
+let fill_color = "#80B7F9";
+```
+
+4. 循环 `标记点的经纬度坐标`, 创建坐标点
+```js
+// new Graphic: 创建新的图形对象
+// new Point: 根据经纬度创建点
+arrayUtils.forEach(point, function (point) {
+  var graphic = new Graphic(
+    new Point(point),
+    createSymbol(iconpath, fill_color)
+  );
+  self.map.graphics.add(graphic);
+});
+```
+
+5. 在图层上绘制点或者多点
+```js
+function createSymbol(path, color) {
+  // new esri.symbol.SimpleMarkerSymbol: 在图层上绘制点
+  var markerSymbol = new esri.symbol.SimpleMarkerSymbol();
+  markerSymbol.setPath(path); // 设置点的样式
+  markerSymbol.setColor(new dojo.Color(color)); // 设置点的填充颜色
+  markerSymbol.setOutline(null); // 设置点的轮廓
+  return markerSymbol;
+}
 ```
--- src/components/Map/index.vue
+++ src/components/Map/index.vue
@@ -6,6 +6,11 @@
 import * as esriLoader from "esri-loader"; // 引入 esri-loader
 export default {
   name: "Map",
+  data() {
+    return {
+      map: null,
+    };
+  },
   created() {
     this.init(); // 初始化地图
   },
@@ -16,13 +21,58 @@ export default {
         url: "https://js.arcgis.com/3.39/",
       };
 
-      esriLoader.loadModules(["esri/map", "dojo/domReady!"], options).then(([Map]) => {
-        this.map = new Map("Map", {
-          basemap: "topo-vector",
-          center: [19.82, 41.33],
-          zoom: 3,
+      esriLoader
+        .loadModules(
+          [
+            "esri/map",
+            "esri/geometry/Point",
+            "esri/graphic",
+            "dojo/_base/array",
+            "dojo/domReady!",
+          ],
+          options
+        )
+        .then(([Map, Point, Graphic, arrayUtils]) => {
+          let self = this; // 存储当前的 this 指向
+
+          // arcgis 配置
+          this.map = new Map("Map", {
+            basemap: "topo-vector",
+            center: [19.82, 41.33],
+            zoom: 3,
+          });
+
+          // 当地图加载完成后，执行 mapLoaded 函数
+          this.map.on("load", mapLoaded);
+
+          function mapLoaded() {
+            // 标记点的经纬度坐标
+            let point = [
+              [19.82, 41.33],
+              [16.37, 48.21],
+            ];
+            // 标记点的图片(svg)
+            let iconpath =
+              "M819.6 606.7l-185.8-52.5-189 52.6-103.5-42.9-28.7 69.3L440 686l193.6-53.9 190.9 54 136.7-57.7-29.2-69.1zM819.6 820.9l-185.8-52.5-189 52.6-103.5-42.8-28.7 69.2L440 900.2l193.6-53.9 190.9 54 136.7-57.7-29.2-69.1zM633.6 203.7l190.9 54 136.7-57.8-29.2-69.1-112.4 47.5-185.8-52.5-189 52.6-103.5-42.9-28.7 69.3L440 257.6zM819.6 392.5L633.8 340l-189 52.6-103.5-42.9-28.7 69.3L440 471.8l193.6-53.9 190.9 54 136.7-57.8-29.2-69zM66.8 139.1h94.9v176.4H66.8v75h94.9v186.6H66.8v75h94.9v201.7H64.5v75h172.2V64.1H66.8z";
+            let fill_color = "#80B7F9";
+
+            arrayUtils.forEach(point, function (point) {
+              var graphic = new Graphic(
+                new Point(point),
+                createSymbol(iconpath, fill_color)
+              );
+              self.map.graphics.add(graphic);
+            });
+          }
+
+          function createSymbol(path, color) {
+            var markerSymbol = new esri.symbol.SimpleMarkerSymbol();
+            markerSymbol.setPath(path);
+            markerSymbol.setColor(new dojo.Color(color));
+            markerSymbol.setOutline(null);
+            return markerSymbol;
+          }
         });
-      });
     },
   },
 };
