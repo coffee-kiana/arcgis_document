be835f163efb7b2292308c95e825846e736622f2
xuan <xuan>
2022-01-18 16:16:01 +0800

    对图层的操作

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,87 @@
 <template>
   <div id="app">
-    <Map/>
+    <div class="button_box">
+      <div class="button" @click="handle_render">渲染</div>
+      <div class="button" @click="handle_switch">切换</div>
+      <div class="button" @click="handle_add">增加</div>
+      <div class="button" @click="handle_remove">清除</div>
+    </div>
+    <Map :point="point" :operation="operation" />
   </div>
 </template>
 
 <script>
-import Map from './components/Map'
+import Map from "./components/Map";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Map
-  }
-}
+    Map,
+  },
+  data() {
+    return {
+      operation: '',
+      point: [],
+    };
+  },
+  methods: {
+    handle_render() {
+      this.operation='render'
+      this.point = [[19.82, 41.33]];
+    },
+    handle_switch() {
+      this.operation='switch'
+      this.point = [[16.37, 48.21]];
+    },
+    handle_add() {
+      this.operation='add'
+      this.point = this.point.concat([
+        [14.51, 46.0],
+        [12.45, 41.9],
+      ]);
+    },
+    handle_remove() {
+      this.operation='remove'
+      this.point = []
+    },
+  },
+};
 </script>
 
 <style>
-*{
+* {
   margin: 0;
   padding: 0;
 }
 html,
 body,
-#app{
+#app {
   width: 100%;
   height: 100%;
 }
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+.button_box {
+  position: absolute;
+  right: 30px;
+  top: 30px;
+  width: 60px;
+  z-index: 1;
+}
+.button {
+  width: 60px;
+  height: 60px;
+  border: 1px solid #ddd;
+  border-radius: 9px;
+  cursor: pointer;
+  line-height: 60px;
+  font-size: 14px;
+  background-color: aliceblue;
+  margin: 12px 0;
 }
 </style>
--- src/components/Map/index.vue
+++ src/components/Map/index.vue
@@ -3,24 +3,33 @@
 </template>
 
 <script>
-import * as esriLoader from "esri-loader"; // 引入 esri-loader
+import * as esriLoader from "esri-loader";
 export default {
   name: "Map",
+  props: ["point", "operation"],
   data() {
     return {
       map: null,
+      pointLayer: null,
+      Graphic: null,
+      Point: null,
+      SimpleMarkerSymbol: null,
+      Color: null,
+      arrayUtils: null,
+      options: {
+        url: "https://js.arcgis.com/3.39/",
+      },
+      infoTemplate: null,
+      fill_color: "#80B7F9",
+      iconpath:
+        "M528 32C325.056 32 160 196.8 160 399.36c0 75.2 22.656 147.584 65.024 208.48 2.112 3.648 4.256 7.168 6.784 10.592l268.608 353.472c7.296 8.096 17.088 12.576 27.584 12.576 10.368 0 20.224-4.512 28.768-14.08l267.36-352c2.624-3.52 4.896-7.36 6.112-9.6A364.864 364.864 0 0 0 896 399.36C896 196.8 730.912 32 528 32z m0 498.72a131.52 131.52 0 0 1-131.456-131.232 131.488 131.488 0 0 1 262.88 0 131.52 131.52 0 0 1-131.424 131.2z",
     };
   },
   created() {
-    this.init(); // 初始化地图
+    this.init();
   },
   methods: {
     init() {
-      // arcgis的配置
-      const options = {
-        url: "https://js.arcgis.com/3.39/",
-      };
-
       esriLoader
         .loadModules(
           [
@@ -32,75 +41,76 @@ export default {
             "esri/InfoTemplate",
             "dojo/domReady!",
           ],
-          options
+          this.options
         )
         .then(
           ([Map, Point, Graphic, arrayUtils, GraphicsLayer, InfoTemplate]) => {
-            let self = this;
-            // arcgis 配置
+            this.Point = Point;
+            this.Graphic = Graphic;
+            this.arrayUtils = arrayUtils;
+            this.GraphicsLayer = GraphicsLayer;
+            this.InfoTemplate = InfoTemplate;
+            this.SimpleMarkerSymbol = esri.symbol.SimpleMarkerSymbol;
+            this.Color = dojo.Color;
+
             this.map = new Map("Map", {
               basemap: "topo-vector",
               center: [19.82, 41.33],
               zoom: 3,
             });
 
-            let pointLayer = new GraphicsLayer({
+            // 创建图层
+            this.pointLayer = new GraphicsLayer({
               id: "pointLayer",
             });
 
-            let infoTemplate = new InfoTemplate("title", `content`);
-            pointLayer.setInfoTemplate(infoTemplate);
-            // 当地图加载完成后，执行 mapLoaded 函数
-            this.map.on("load", mapLoaded);
+            this.infoTemplate = new InfoTemplate("title", `content`);
+            this.pointLayer.setInfoTemplate(this.infoTemplate);
 
-            function mapLoaded() {
-              // 标记点的经纬度坐标
-              let point = [
-                [19.82, 41.33],
-                [16.37, 48.21],
-              ];
-              // 标记点的图片(svg)
-              let iconpath =
-                "M528 32C325.056 32 160 196.8 160 399.36c0 75.2 22.656 147.584 65.024 208.48 2.112 3.648 4.256 7.168 6.784 10.592l268.608 353.472c7.296 8.096 17.088 12.576 27.584 12.576 10.368 0 20.224-4.512 28.768-14.08l267.36-352c2.624-3.52 4.896-7.36 6.112-9.6A364.864 364.864 0 0 0 896 399.36C896 196.8 730.912 32 528 32z m0 498.72a131.52 131.52 0 0 1-131.456-131.232 131.488 131.488 0 0 1 262.88 0 131.52 131.52 0 0 1-131.424 131.2z";
-              let fill_color = "#80B7F9";
-
-              arrayUtils.forEach(point, function (point, index) {
-                pointLayer.add(
-                  new Graphic(
-                    new Point({
-                      latitude: point[1],
-                      longitude: point[0],
-                      cache: {
-                        index,
-                      },
-                    }),
-                    createSymbol(iconpath, fill_color)
-                  )
-                );
-              });
-
-              self.map.addLayer(pointLayer, 1);
-
-              pointLayer.on("click", function (e) {
-                const { index } = e.graphic.geometry.cache;
-                infoTemplate.setContent(`当前的序号为 ${index}`);
-              });
-            }
-
-            function createSymbol(path, color) {
-              var markerSymbol = new esri.symbol.SimpleMarkerSymbol();
-              markerSymbol.setPath(path);
-              markerSymbol.setColor(new dojo.Color(color));
-              markerSymbol.setOutline(null);
-              return markerSymbol;
-            }
+            this.map.on("load", this.mapLoaded);
           }
         );
     },
+    mapLoaded() {
+      this.deal_with(this.point);
+      this.map.addLayer(this.pointLayer, 1);
+    },
+    // 处理标记点数据
+    deal_with(points) {
+      let self = this;
+      this.arrayUtils.forEach(points, function (point, index) {
+        self.add_point(point)
+      });
+    },
+    add_point(point) {
+      this.pointLayer.add(
+        new this.Graphic(
+          new this.Point({
+            latitude: point[1],
+            longitude: point[0],
+          }),
+          this.createSymbol()
+        )
+      );
+    },
+    createSymbol() {
+      let markerSymbol = new this.SimpleMarkerSymbol();
+      markerSymbol.setPath(this.iconpath);
+      markerSymbol.setColor(new this.Color(this.fill_color));
+      markerSymbol.setOutline(null);
+      return markerSymbol;
+    },
   },
+  watch: {
+    operation: function () {
+      if(this.operation !== 'add'){
+        this.pointLayer.clear(); // 清除该图层的标记点
+      }
+      this.deal_with(this.point)
+    }
+  }
 };
 </script>
-
 
 <style scoped>
 /* 3.39版本的地图样式 */
